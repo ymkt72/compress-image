61ad793c47fa09b4df811fbb7e94f381121ec96b
yuma kitsu <kitsu@yumanoMac-mini.local>
2026-01-20 08:28:09 +0900

    compress jpg

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"image/jpeg"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("引数が足りません")
+	}
+	imagePath := os.Args[1]
+	w, err := os.Open(imagePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	image, err := jpeg.Decode(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputImagePath := os.Args[2]
+	nw, err := os.Create(outputImagePath)
+
+	if err := jpeg.Encode(nw, image, &jpeg.Options{Quality: 60}); err != nil {
+		log.Fatal(err)
+	}
+}
